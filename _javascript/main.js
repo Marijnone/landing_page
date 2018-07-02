637dc42b41630363e9c6c936fd79cd0acae17436
Marijnone <marijn.moviat@hva.nl>
2018-07-02 17:47:37 +0200

    Repsonsive menu in the making

--- _javascript/main.js
+++ _javascript/main.js
@@ -1,3 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('Hello Bulma!');
 });
+//toggle func
+(function() {
+  var burger = document.querySelector('.burger');
+  var nav = document.querySelector('#'+burger.dataset.target);
+ 
+  burger.addEventListener('click', function(){
+    burger.classList.toggle('is-active');
+    nav.classList.toggle('is-active');
+  });
+})();
